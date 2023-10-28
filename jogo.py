5e71e783a50fdab69296c773bf5ceaefed24e314
Giovana Z Corsetti <giovana.corsetti@outlook.com>
2023-10-28 12:07:56 -0300

    commit p apresentação

--- jogo.py
+++ jogo.py
@@ -8,7 +8,7 @@ def animacao_protagonista():
 
     # Animação do personagem andando no chão, pulando e atacando.
     # Só ataca quando está no chão.
-    if(protagonista_retangulo.bottom < 470):
+    if(protagonista_retangulo.bottom < 440):
         protagonista_superficies = protagonista_pulando_superficie[int(protagonista_index)]
     else: 
         protagonista_index += 0.1
@@ -24,12 +24,15 @@ def obstaculos(lista_obstaculos):
     # Confere se há algum obstáculo na lista.
     if lista_obstaculos:
         for obstaculo_retangulo in lista_obstaculos:
-            obstaculo_retangulo.x -= 5
 
             # Se for no chão, faz aparecer os spikes.
-            if obstaculo_retangulo.bottom == 470: tela.blit(spikes_superficie, obstaculo_retangulo)
+            if obstaculo_retangulo.bottom == 490: 
+                tela.blit(spikes_superficie, obstaculo_retangulo)
+                obstaculo_retangulo.x -= 7
             # Se for no ar, aparece as pedras voadoras.
-            else: tela.blit(pedras_superficie, obstaculo_retangulo)
+            else: 
+                tela.blit(pedras_superficie, obstaculo_retangulo)
+                obstaculo_retangulo.x -= 5
 
         lista_obstaculos = [obstaculo for obstaculo in lista_obstaculos if obstaculo.x > -100]
         return lista_obstaculos
@@ -61,7 +64,7 @@ plano_fundo = pygame.image.load('assets/fundo/fundo-sem-camadas.png').convert()
 
 # IMPORTANDO ARQUIVOS DE FONTES
 youngserif_font = pygame.font.Font('assets/fonts/YoungSerif-Regular.ttf', 80)
-youngserif_peq_font = pygame.font.Font('assets/fonts/YoungSerif-Regular.ttf', 50)
+youngserif_peq_font = pygame.font.Font('assets/fonts/YoungSerif-Regular.ttf', 40)
 
 # DEIXANDO OS TAMANHOS DE ACORDO COM O TAMANHO DA TELA
 plano_fundo = pygame.transform.scale(plano_fundo, tamanho_tela)
@@ -107,7 +110,8 @@ for imagem in range(1, 5):
 
 # RETÂNGULO DE COLISÃO DO PROTAGONISTA
 protagonista_superficies = protagonista_andando_superficie[protagonista_index]
-protagonista_retangulo = protagonista_superficies.get_rect(midbottom = (100, 470))
+protagonista_retangulo = protagonista_superficies.get_rect(midbottom = (100, 450))
+protagonista_retangulo = protagonista_retangulo.inflate(-36, -62)
 protagonista_gravidade = 0
 
 
@@ -121,13 +125,13 @@ spikes_index = 0
 for imagem in range(1, 4):
     img = pygame.image.load(f'assets/boss/ataque-chao/boss_groundslam{imagem}.png').convert_alpha()
     # Redimensionando para ficar do mesmo tamanho das pedras.
-    img = pygame.transform.scale(img, (64, 64))
+    img = pygame.transform.scale(img, (108, 108))
     spikes_frames_superficie.append(img)
 
 # Pedras voadoras
 pedras_index = 0
-pedras_superficie = pygame.image.load(f'assets/boss/ataque-pedra/cast1.png').convert_alpha()
-pedras_superficie = pygame.transform.scale(pedras_superficie, (64, 64))
+pedras_superficie = pygame.image.load(f'assets/boss/ataque-pedra/pedracast1.png').convert_alpha()
+pedras_superficie = pygame.transform.scale(pedras_superficie, (84, 84))
 
 spikes_superficie = spikes_frames_superficie[spikes_index]
 lista_retang_obstaculos = []
@@ -135,13 +139,13 @@ lista_retang_obstaculos = []
 
 # CRIA O TIMER DOS OBSTÁCULOS
 obstaculos_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstaculos_timer, 1400)
+pygame.time.set_timer(obstaculos_timer, 900)
 
 spikes_timer = pygame.USEREVENT + 2
-pygame.time.set_timer(spikes_timer, 500)
+pygame.time.set_timer(spikes_timer, 320)
 
 pedras_timer = pygame.USEREVENT + 3
-pygame.time.set_timer(pedras_timer, 50)
+pygame.time.set_timer(pedras_timer, 10)
 
 
 
@@ -163,7 +167,7 @@ while True:
             jogo_rodando = True
             # CONTROLA O PULO DO PROTAGONISTA
             if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_SPACE and protagonista_retangulo.bottom >= 470:
+                if evento.key == pygame.K_SPACE and protagonista_retangulo.bottom >= 440:
                     protagonista_gravidade = -19
 
             if evento.type == pygame.KEYUP:
@@ -173,9 +177,13 @@ while True:
             if evento.type == obstaculos_timer:
                 if randint(0, 2):
                     # Retângulo dos spikes.
-                    lista_retang_obstaculos.append(spikes_superficie.get_rect(bottomright = (randint(960, 1000), 470)))
+                    spikes_retangulo = spikes_superficie.get_rect(bottomright = (randint(960, 1000), 490))
+                    spikes_retangulo = spikes_retangulo.inflate(-62, 0)
+                    lista_retang_obstaculos.append(spikes_retangulo)
                     # Retângulo das pedras.
-                    lista_retang_obstaculos.append(pedras_superficie.get_rect(bottomright = (randint(960, 1200), 170)))
+                    pedras_retangulo = pedras_superficie.get_rect(bottomright = (randint(960, 1200), 188))
+                    pedras_retangulo = pedras_retangulo.inflate(-5, -5)
+                    lista_retang_obstaculos.append(pedras_retangulo)
         
             if evento.type == spikes_timer:
                 if spikes_index == 0: spikes_index = 1
@@ -198,7 +206,7 @@ while True:
         # CHAMA A ANIMAÇÃO DO PROTAGONISTA
         protagonista_gravidade += 0.9
         protagonista_retangulo.y += protagonista_gravidade
-        if protagonista_retangulo.bottom >= 470: protagonista_retangulo.bottom = 470
+        if protagonista_retangulo.bottom >= 440: protagonista_retangulo.bottom = 440
         animacao_protagonista()
 
         # CHAMA A FUNÇÃO DOS OBSTÁCULOS SE MOVENDO
@@ -210,9 +218,15 @@ while True:
     # TELA DE GAME OVER
     else:
         tela.blit(plano_fundo, (0,0))
+        # texto de game over
         gameover_texto = youngserif_font.render(f'game over', False, (255, 255, 255))
-        gameover_texto_retangulo = gameover_texto.get_rect(center = (480, 260))
+        gameover_texto_retangulo = gameover_texto.get_rect(center = (480, 150))
         tela.blit(gameover_texto, gameover_texto_retangulo)
+        # texto de 'tente novamente'
+        restart_texto = youngserif_peq_font.render(f'aperte ESPAÇO para jogar novamente', False, (255, 255, 255))
+        restart_texto_retangulo = restart_texto.get_rect(center = (480, 250))
+        tela.blit(restart_texto, restart_texto_retangulo)
+        
 
 
     # ATUALIZA A TELA COM O CONTEÚDO
